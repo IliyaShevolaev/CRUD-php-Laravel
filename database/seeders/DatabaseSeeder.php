3ccc028f9938c681a02c2f627e0c4879a28dd2e8
iliyashev <iliya.shevolaev@gmail.com>
2025-07-22 11:26:47 +0300

    osp update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        
-        Category::truncate();
+
+        DB::table('categories')->delete();
         $categories = ['Buy', 'Sale', 'Rent'];
         foreach($categories as $category) {
             Category::create(['name' => $category]);
